7e9c468b6a42c799467ee2bd7414e0484e87de47
rgast <rgast@cbs.mpg.de>
2019-12-13 18:41:03 +0100

    ODE approximation of stn-gpe system with discrete delays (8th order gamma distribution)

--- BasalGanglia/stn_gpe_single_run.py
+++ BasalGanglia/stn_gpe_single_run.py
@@ -6,19 +6,31 @@ from numba import jit
 
 
 # parameters
-dt = 1e-4
+dt = 5e-5
 T = 2.0
 dts = 1e-3
 
-eic = CircuitIR.from_yaml("config/stn_gpe/net_qif_syn_adapt").compile(backend='numpy', step_size=dt, solver='scipy')
+eic = CircuitIR.from_yaml("config/stn_gpe/net_qif_syn_adapt").compile(backend='numpy', step_size=dt, solver='euler')
 results, t = eic.run(simulation_time=T, sampling_step_size=dts, profile=True,
                      outputs={'r_e': 'stn/qif_stn/R_e',
                               'r_i': 'gpe/qif_gpe/R_i',
-                              'v_e': 'stn/qif_stn/V_e',
-                              'v_i': 'gpe/qif_gpe/V_i',
+                              #'v_e': 'stn/qif_stn/V_e',
+                              #'v_i': 'gpe/qif_gpe/V_i',
                               }
                      )
 
 #print(results.iloc[-1, :])
 results.plot()
+
+eic2 = CircuitIR.from_yaml("config/stn_gpe/net_stn_gpe").compile(backend='numpy', step_size=dt, solver='scipy')
+results2, t = eic2.run(simulation_time=T, sampling_step_size=dts, profile=True,
+                       outputs={'r_e': 'stn_gpe/qif_full/R_e',
+                                'r_i': 'stn_gpe/qif_full/R_i',
+                                #'v_e': 'stn_gpe/qif_full/V_e',
+                                #'v_i': 'stn_gpe/qif_full/V_i',
+                                }
+                       )
+
+#print(results.iloc[-1, :])
+results2.plot()
 plt.show()
