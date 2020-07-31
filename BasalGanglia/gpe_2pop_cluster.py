905b16d669e1e39a6bef5a5804e473b130f9c7e0
rgast <rgast@cbs.mpg.de>
2020-07-31 10:29:20 +0200

    added resource control parameters to gpe_2pop_cluster.py cluster grid search call

--- BasalGanglia/gpe_2pop_cluster.py
+++ BasalGanglia/gpe_2pop_cluster.py
@@ -122,7 +122,7 @@ res_file = cgs.run(
             chunk_size=chunk_size,
             worker_env="/data/u_rgast_software/anaconda3/envs/pyrates/bin/python3",
             worker_file=f'{os.getcwd()}/stn_gpe_syns_worker.py',
-            worker_kwargs={},
+            worker_kwargs={'time_lim': 4000.0, 'cpu_lim': True, 'nproc_lim': False, 'memory_lim': False},
             gs_kwargs={'init_kwargs': {'backend': 'numpy', 'solver': 'scipy', 'step_size': dt, 'matrix_sparseness': 1.0}
                        },
             method='RK45',
